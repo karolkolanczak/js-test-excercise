b5c774a0c88a3a11d0687d5ed970522332848390
Karol <karol.kolanczak@hotmail.com>
2018-05-15 13:47:17 +0200

    adding function to display details after clicking on particular user

--- file.js
+++ file.js
@@ -1,4 +1,16 @@
 
+const addElemListener = (elem) => {
+    elem.addEventListener('click', (e) => {
+        var id = elem.dataset.id;
+        var cont = document.getElementById('details');
+        // cont.innerText = id;
+        fetch('https://isa-simple-rest-api.herokuapp.com/api/users/' + id)
+            .then(response => response.json())
+            .then((data) => {
+                cont.innerHTML = '<h2>' + data.id +  ' # ' + data.username + '</h2>';
+            });
+    }, false);
+}
 
 const getUsers = ()=>{
     return fetch('https://isa-simple-rest-api.herokuapp.com/api/users')
@@ -9,10 +21,15 @@ const getUsers = ()=>{
             for(var value of data) {
                 console.log(value.id + ' - ' + value.username);
                 var user = document.createElement('li');
+                user = document.createElement("BUTTON");
                 user.innerText = value.id + ' - ' + value.username;
+                user.dataset.id = value.id;
                 authors.appendChild(user);
+                addElemListener(user);
             }
         });
 }
 
+
 getUsers();
+
